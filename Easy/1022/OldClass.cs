4b6e2bd4faead41e2f90def16dce0f095aea1e32
Андрей Барышников <andrey1andrey@yandex.ru>
2026-03-08 20:55:27 +0300

    1022

---
+++ Easy/1022/OldClass.cs
@@ -0,0 +1,114 @@
+﻿//string input;
+//input = Console.ReadLine();
+////input = "root = [1,0,1,0,1,0,1]";
+////input = "root = [0]";
+//int index = input.IndexOf('[') + 1;
+////Console.WriteLine(index);
+//input = input.Substring(index, input.Length - index);
+////Console.WriteLine(input);
+//string[] strArr = input.Split(',', ']');
+////foreach (var item in strArr)
+////{
+////    Console.WriteLine(item);
+////}
+//int count = strArr.Length - 1;
+////Console.WriteLine(count);
+//var queue = new Queue<TreeNode>(count);
+//int poz = 0;
+//var dict = new Dictionary<string, int>();
+//var root = CreateNodeTreeAndSaveVal(strArr, poz++, count, "");
+//while (poz < count)
+//{
+//    var node = queue.Dequeue();
+//    node.left = CreateNodeTreeAndSaveVal(strArr, poz++, count, node.calcValStr);
+//    if (poz < count)
+//        node.right = CreateNodeTreeAndSaveVal(strArr, poz++, count, node.calcValStr);
+//    else
+//        break;
+//}
+//int result = 0;
+//Calc(root);
+////Console.WriteLine();
+////Console.WriteLine("--");
+//Console.WriteLine(result);
+
+//void Calc(TreeNode node)
+//{
+//    if (node.left == null && node.right == null)
+//    {
+//        result += node.calcVal;
+//    }
+//    if (node.left != null)
+//    {
+//        Calc(node.left);
+//    }
+//    if (node.right != null)
+//    {
+//        Calc(node.right);
+//    }
+//}
+
+//TreeNode CreateNodeTreeAndSaveVal(string[] strArr, int poz, int count, string calcValStr)
+//{
+//    var node = new TreeNode();
+//    if (poz < count)
+//    {
+//        node.val = Convert.ToInt32(strArr[poz]);
+//        if (string.IsNullOrEmpty(calcValStr))
+//        {
+//            node.calcValStr = node.val.ToString();
+//        }
+//        else
+//        {
+//            node.calcValStr = calcValStr + node.val.ToString();
+//        }
+//        node.calcVal = ToInt(node.calcValStr);
+//        queue.Enqueue(node);
+//        return node;
+//    }
+//    return null;
+//}
+
+//int ToInt(string calcValStr)
+//{
+//    int val = 0;
+//    if (!dict.TryGetValue(calcValStr, out val))
+//    {
+//        int count = calcValStr.Length - 1;
+//        int pow = 1;
+//        for (int i = 0; i < calcValStr.Length; i++)
+//        {
+//            if (calcValStr[count - i] == '1')
+//            {
+//                val += pow;
+//            }
+//            pow *= 2;
+//        }
+//        dict[calcValStr] = val;
+//    }
+//    return val;
+//}
+
+//// Definition for a binary tree node.
+//public class TreeNode
+//{
+//    public int val;
+//    public int calcVal;
+//    public string calcValStr;
+//    public TreeNode left;
+//    public TreeNode right;
+//    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+//    {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+
+//public class Solution
+//{
+//    public int SumRootToLeaf(TreeNode root)
+//    {
+//        return 0;
+//    }
+//}
---
+++ Easy/1022/Program.cs
@@ -0,0 +1,93 @@
+﻿public class Solution
+{
+    static Dictionary<string, int> dict;
+    static TreeNode root;
+    static int result;
+    static Queue<TreeNode> queue;
+
+    public static void Main()
+    {
+        string input;
+        //input = Console.ReadLine();
+        input = "root = [1,0,1,0,1,0,1]";
+        //input = "root = [0]";
+        int index = input.IndexOf('[') + 1;
+        Console.WriteLine(index);
+        input = input.Substring(index, input.Length - index);
+        Console.WriteLine(input);
+        string[] strArr = input.Split(',', ']');
+        foreach (var item in strArr)
+        {
+            Console.WriteLine(item);
+        }
+        int count = strArr.Length - 1;
+        Console.WriteLine(count);
+        queue = new Queue<TreeNode>(count);
+        int poz = 0;
+        dict = new Dictionary<string, int>();
+        root = CreateNodeTreeAndSaveVal(strArr, poz++, count);
+        while (poz < count)
+        {
+            var node = queue.Dequeue();
+            node.left = CreateNodeTreeAndSaveVal(strArr, poz++, count);
+            if (poz < count)
+                node.right = CreateNodeTreeAndSaveVal(strArr, poz++, count);
+            else
+                break;
+        }
+        result = 0;
+        var sol = new Solution();
+        result = sol.SumRootToLeaf(root);
+        Console.WriteLine();
+        Console.WriteLine("--");
+        Console.WriteLine(result);
+    }
+
+    static TreeNode CreateNodeTreeAndSaveVal(string[] strArr, int poz, int count)
+    {
+        var node = new TreeNode();
+        if (poz < count)
+        {
+            node.val = Convert.ToInt32(strArr[poz]);
+            queue.Enqueue(node);
+            return node;
+        }
+        return null;
+    }
+
+    // Definition for a binary tree node. 
+    public int SumRootToLeaf(TreeNode root)
+    {
+        int result = 0;
+        Calc(root, 0, ref result);
+        return result;
+    }
+    void Calc(TreeNode node, int val, ref int result)
+    {
+        if (node.left == null && node.right == null)
+        {
+            result += val * 2 + node.val;
+        }
+        if (node.left != null)
+        {
+            Calc(node.left, val * 2 + node.val, ref result);
+        }
+        if (node.right != null)
+        {
+            Calc(node.right, val * 2 + node.val, ref result);
+        }
+    }
+
+}
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
