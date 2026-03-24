6c96221125bbed6caa2bb01c63827a3a009c8d31
Андрей Барышников <andrey1andrey@yandex.ru>
2026-03-24 21:12:59 +0300

    0014

---
+++ Easy/0014/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        string[] s = ["MD"];
+        var sol = new Solution();
+        string result;
+        result = sol.LongestCommonPrefix(s);
+        Console.WriteLine(result);
+    }
+    public string LongestCommonPrefix(string[] strs)
+    {
+
+        return "";
+    }
+}
