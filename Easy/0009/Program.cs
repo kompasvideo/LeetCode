9e10e9bab05662709a363a5bcbafbb6abef9b55a
Андрей Барышников <andrey1andrey@yandex.ru>
2026-03-24 21:01:19 +0300

    0013

---
+++ Easy/0009/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        int x = 10;
+        var sol = new Solution();
+        bool result;
+        result = sol.IsPalindrome(x);
+        Console.WriteLine(result);
+    }
+    public bool IsPalindrome(int x)
+    {
+        int[] mas = new int[64];
+        int i = 0;
+        if (x < 0)
+        {
+            return false;
+        }
+        while (true)
+        {
+            mas[i++] = x % 10;
+            if (x >= 10)
+                x = x / 10;
+            else
+                break;
+        }
+        i--;
+        int k = 0;
+        while (true)
+        {
+            if (i < k)
+            {
+                if (k - i == 1)
+                    return true;
+                return false;
+            }
+            if (mas[i] != mas[k])
+                return false;
+            if (i == k)
+                return true;
+            i--;
+            k++;
+        }
+    }
+}
