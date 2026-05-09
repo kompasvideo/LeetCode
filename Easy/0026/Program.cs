9bd240e22fbb6e85d8b4033a5e96439165ea829d
Андрей Барышников <andrey1andrey@yandex.ru>
2026-05-10 01:02:04 +0300

    0026

---
+++ Easy/0026/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        int[] nums = [1, 2, 4];
+        var sol = new Solution();
+        var result = sol.RemoveDuplicates(nums);
+        Console.WriteLine(result);
+    }
+
+    public int RemoveDuplicates(int[] nums)
+    {
+        int k = 0;
+        int n = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != n)
+            {
+                int t = nums[i];
+
+                n = nums[i];
+            }
+            k++;
+        }
+        return 0;
+    }
+}
