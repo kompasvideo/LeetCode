de8277a5d70717cc9f49f6158424d65a43c0af20
kompasvideo <andrey1andrey@yandex.ru>
2026-06-29 23:11:14 +0300

    0069

---
+++ Easy/0069/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        int a = 4;
+        var sol = new Solution();
+        var result = sol.MySqrt( a);
+        Console.WriteLine(result);
+    }
+    public int MySqrt(int x)       
+    {
+
+        return 0;
+    }
+}
