8d4479852675a244de48226e9d7a5830681f0ff5
kompasvideo <andrey1andrey@yandex.ru>
2026-07-20 23:52:30 +0300

    0069

--- Easy/0069/Program.cs
+++ Easy/0069/Program.cs
@@ -2,14 +2,27 @@
 {
     public static void Main()
     {
+        //int a = 2147395599;
         int a = 4;
         var sol = new Solution();
-        var result = sol.MySqrt( a);
+        var result = sol.MySqrt(a);
         Console.WriteLine(result);
     }
     public int MySqrt(int x)       
     {
-
-        return 0;
+        uint right = 46340;
+        uint left = 0;
+        long middle = 0;
+        while (right - left > 1)
+        {
+            middle = (right + left + 0l) / 2;
+            if (middle * middle < x)
+                left = (uint)middle;
+            else
+                right = (uint)middle;
+        }
+        if ((left + 1) * (left + 1) <= x)
+            return (int)(left + 1);
+        return (int)left;
     }
 }
