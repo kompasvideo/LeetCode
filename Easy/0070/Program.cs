733b99fbe4caea4d67e3f9a1841650772435a6ee
kompasvideo <andrey1andrey@yandex.ru>
2026-07-21 00:01:02 +0300

    0070

---
+++ Easy/0070/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        //int a = 2147395599;
+        int a = 2;
+        var sol = new Solution();
+        var result = sol.ClimbStairs(a);
+        Console.WriteLine(result);
+    }
+    public int ClimbStairs(int n)
+    {
+        
+        return 0;
+    }
+}
