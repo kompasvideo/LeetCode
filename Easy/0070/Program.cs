bcce7853d06146d739b56bca643f578a6ffd3bc0
kompasvideo <andrey1andrey@yandex.ru>
2026-07-25 19:33:26 +0300

    0070

--- Easy/0070/Program.cs
+++ Easy/0070/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Solution
+﻿using System.Runtime.ConstrainedExecution;
+
+public class Solution
 {
     public static void Main()
     {
@@ -10,7 +12,21 @@
     }
     public int ClimbStairs(int n)
     {
-        
-        return 0;
+        if (n <= 0)
+            return 1;
+        else if (n == 1)
+            return 1;
+
+        int a = 1, b = 1;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
     }
 }
+
