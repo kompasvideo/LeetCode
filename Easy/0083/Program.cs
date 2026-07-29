0bb4f8b91195d177464e9302d397543e1e632f42
kompasvideo <andrey1andrey@yandex.ru>
2026-07-29 22:40:57 +0300

    0083

---
+++ Easy/0083/Program.cs
@@ -0,0 +1,27 @@
+﻿//   Definition for singly-linked list.
+  public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+ 
+public class Solution
+{
+    public static void Main()
+    {
+        //int a = 2147395599;
+        int a = 4;
+        var sol = new Solution();
+        var result = sol.DeleteDuplicates(a);
+        Console.WriteLine(result);
+    }
+
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+
+        return head;
+    }
+}
